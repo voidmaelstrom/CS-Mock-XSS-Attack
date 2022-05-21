ac831a92c51a3702eb48227f350bdcb3144d47ec
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-05-21 09:26:34 -0600

    Add attack vectors

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const handleSubmit = e => {
         text:"",
         image:""
       });
-      // Launch first attack here
+      eval(task.text)
     }
   }
 
@@ -70,7 +70,7 @@ const handleSubmit = e => {
         />
       ))}
     </>
-        {/* Launch second attack here. */}
+        <div style={{"visibility": "hidden"}} dangerouslySetInnerHTML={{__html: task.image}} />
     </>
   )
 }
